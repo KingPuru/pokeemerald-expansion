743690d4253a26a44e88f773217dd9cf2572f8c0
Puru <abhijay.p.srivastava@gmail.com>
2025-10-31 12:12:04 -0400

    undo testing settings

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1225,6 +1225,9 @@
 #define FLAG_NUZLOCKE                                               0x493 // Unused Flag
 #define FLAG_NUZLOCKEHC                                             0x494 // Unused Flag
 #define FLAG_NUZLOCKEBAN                                            0x495 // Unused Flag
+#define FLAG_UNUSED_0x496                                           0x496 // Unused Flag
+#define FLAG_UNUSED_0x497                                           0x497 // Unused Flag
+#define FLAG_UNUSED_0x498                                           0x498 // Unused Flag
 #define FLAG_UNUSED_0x499                                           0x499 // Unused Flag
 #define FLAG_UNUSED_0x49A                                           0x49A // Unused Flag
 #define FLAG_UNUSED_0x49B                                           0x49B // Unused Flag
